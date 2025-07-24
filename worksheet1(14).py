d08b438db1f0cc62aa3ef4315905bc5c12a7443e
ADMIN <kavyalipseka98@gmail.com>
2025-07-24 17:47:27 +0530

    push

---
+++ worksheet1(14).py
@@ -0,0 +1,7 @@
+j,k=37,52;
+print("the values of j and k :",j,k);
+print("the sum of j and k is:",j+k);
+j,k=8,3;
+print("the values of j and k after re assigning:",j,k);
+print("the sum is:",j+k);
+print("the product is:",j-k);
