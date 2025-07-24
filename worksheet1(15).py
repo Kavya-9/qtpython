82882f1cfe96eb50519468ed168e052e98d6ca62
ADMIN <kavyalipseka98@gmail.com>
2025-07-24 17:52:55 +0530

    push

---
+++ worksheet1(15).py
@@ -0,0 +1,20 @@
+FirstName = "Raja"
+LastName = "Shekhar"
+PhoneNumber = "9703971577"
+Email = "raj@peramsons.com"
+JavaMarks = 70
+PythonMarks = 90
+Database = 90
+WebDevelopment = 90
+TotalMarks = 340
+Average = 85.0
+print("FirstName:", FirstName, "type:", type(FirstName))
+print("LastName:", LastName, "type:", type(LastName))
+print("PhoneNumber:", PhoneNumber, "type:", type(PhoneNumber))
+print("Email:", Email, "type:", type(Email))
+print("JavaMarks:", JavaMarks, "type:", type(JavaMarks))
+print("PythonMarks:", PythonMarks, "type:", type(PythonMarks))
+print("Database:", Database, "type:", type(Database))
+print("WebDevelopment:", WebDevelopment, "type:", type(WebDevelopment))
+print("TotalMarks:", TotalMarks, "type:", type(TotalMarks))
+print("Average:", Average, "type:", type(Average))
