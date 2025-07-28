cb75074dca9262017d601db42e755b1745f7af78
Kavya-9 <kavyalipseka98@gmail.com>
2025-07-28 16:01:44 +0530

    push

---
+++ ws2-10.py
@@ -0,0 +1,16 @@
+l = 15
+b = 10
+a = l * b
+print("length of Raj's garden:", l, "m")
+print("breadth of Raj's garden:", b, "m")
+print("area of Raj's garden:", a, "sq.m")
+
+s1 = 12
+s2 = 12
+area = s1 * s2
+print("area of Vamshi's garden:", area, "sq.m")
+
+if a > area:
+    print("Hence, Raj's Garden is bigger.")
+else:
+    print("Hence, Vamshi's Garden is bigger.")
