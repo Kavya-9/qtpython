08ed75bf8a4e141985f4e185febb5e9aed5ab804
Kavya-9 <kavyalipseka98@gmail.com>
2025-07-30 16:56:48 +0530

    push

---
+++ d2-7.py
@@ -0,0 +1,2 @@
+myTotal = 8 + 2 * 10
+print("the vale of 8 + 2 * 10 is: ",myTotal)
---
+++ d2-9.py
@@ -0,0 +1,10 @@
+numberOne=16
+numberTwo=4
+answer=numberOne+numberTwo
+print("the sum of numberOne and numberTwo is:",answer)
+answer1 = numberOne + numberTwo + 20
+print("the value of numberOne and numberTwo,20 is ",answer1)
+answer2 = 10 * numberOne + (numberTwo + 20)
+print(answer2)
+answer3 = numberOne + 20 + numberTwo -10
+print(answer3)
